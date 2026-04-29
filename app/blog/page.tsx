c6e56cf0f98c38afee31d9e9361118d4ea037e1d
Zuan-Pemula <azwanazhari200420@gmail.com>
2026-04-29 21:15:52 +0800

    first commit

---
+++ app/blog/page.tsx
@@ -0,0 +1,9 @@
+export default function Blog() {
+    return (
+        <div>
+        <p>ini adalah halaman BLOG!</p>
+        <input type="text" placeholder="Masukkan teks di sini" />
+        <button>Click Me</button>
+        </div>
+    );
+}
---
+++ app/learn1/page.tsx
@@ -0,0 +1,14 @@
+export default function learn() {
+    return (
+         <div>
+      <div>
+        <img></img>
+        <p>desains</p>
+      </div>
+      <div>
+        <h1>figma</h1>
+        <p>8 hours ago</p>
+      </div>
+    </div>
+    );
+}
